0afe5be09e1a026b73584f9e1b493b8843070aa3
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-06-07 20:46:19 -0400

    Con Certificado 2013
    Con modificación para certificado en formato horizontal

--- certificado/total.php
+++ certificado/total.php
@@ -3,7 +3,8 @@ include_once("../login/check.php");
 include_once("../fpdf/fpdf.php");
 class PDF extends FPDF{
 		function Header(){
-		//$this->Image("../images/certificado.jpg",0,0,215,280);	
+			//$this->Image("../images/certificado2013.jpg",0,0,215,280);//Vertical
+			$this->Image("../images/certificado2013.jpg",0,0,280,216);//Horizontal
 			
 		}
 		function Footer()
@@ -12,7 +13,7 @@ class PDF extends FPDF{
 	}
 	include_once("../class/asistencia.php");
 	include_once("../class/alumnos.php");
-	$pdf=new PDF("P","mm","letter");//612,792
+	$pdf=new PDF("L","mm","letter");//612,792
 	$pdf->SetMargins(0,0,0);
 	$pdf->SetAutoPageBreak(true,0);
 	$alumnos=new alumnos;
@@ -22,10 +23,10 @@ class PDF extends FPDF{
 	
 
 	foreach($alumnos->mostrarAsistentes() as $al){
-		if(strlen($al['Paterno']." ".$al['Materno']." ".$al['Nombres'])>35)
-			$pdf->SetFont("Helvetica","",24);
-		else
-			$pdf->SetFont("Helvetica","",26);
+		//if(strlen($al['Paterno']." ".$al['Materno']." ".$al['Nombres'])>35)
+		//	$pdf->SetFont("Helvetica","",24);
+		//else
+			$pdf->SetFont("Helvetica","B",28);
 		
 		$fecha1=$asistencia->verAsistenciaFecha($al['CodAlumno'],"2013-06-10");
 		$fecha2=$asistencia->verAsistenciaFecha($al['CodAlumno'],"2013-06-11");
@@ -33,13 +34,15 @@ class PDF extends FPDF{
 		$fecha4=$asistencia->verAsistenciaFecha($al['CodAlumno'],"2013-06-13");
 		$total=$fecha1['Cantidad']+$fecha2['Cantidad']+$fecha3['Cantidad']+$fecha4['Cantidad'];
 
-	if($total>=3)
+	if($total>=0)
 	{
 		$pdf->AddPage();
-	$pdf->SetXY(25,64);
-	
-	//$al=array("Paterno"=>"Nina","Materno"=>"Layme","Nombres"=>"Ronald Franz");
-	$pdf->Cell(180,12,utf8_decode(ucwords(mb_strtolower($al['Paterno']." ".$al['Materno']." ".$al['Nombres'],"utf8"))),0,0,"C",$relleno);
+		//Apellidos
+		$pdf->SetXY(110,60);
+		$pdf->Cell(160,12,utf8_decode(ucwords(mb_strtolower($al['Paterno']." ".$al['Materno'],"utf8"))),1,0,"C",$relleno);
+		//Nombres
+		$pdf->SetXY(110,75);
+		$pdf->Cell(160,12,utf8_decode(ucwords(mb_strtolower($al['Nombres'],"utf8"))),1,0,"C",$relleno);
 
 	}
 		

50609fbb09431da52c59f8953fc724c215795ccc
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-06-07 23:53:34 -0400

    Correción Clase Alumno2

---
+++ class/alumnos2.php
@@ -0,0 +1,35 @@
+<?php
+include_once("db2.php");
+class alumnos2 extends db{
+	var $tabla="alumnos2";
+	function mostrarTodos(){
+		$this->campos=array("*");
+		return $this->getRecords("","Paterno,Materno,Nombres");
+	}
+	function mostrarAsistentes(){
+		$this->campos=array("*");
+		return $this->getRecords("CodAlumno IN (Select Distinct(CodAlumno) from asistencia WHERE Activo=1)","Paterno,Materno,Nombres");
+			
+	}
+	function mostrarDatos($Ru){
+		$this->campos=array("*");
+		return $this->getRecords("Ru=$Ru or Ci=$Ru");
+	}
+	function mostrarDatosRu($Ru){
+		$this->campos=array("*");
+		return $this->getRecords("Ru=$Ru",0,0,0,0,0,1);
+	}
+	function mostrarDatosXCod($CodAlumno){
+		$this->campos=array("*");
+		return $this->getRecords("CodAlumno=$CodAlumno");
+	}
+	function mostrarDatosXCi($Ci){
+		$this->campos=array("*");
+		return $this->getRecords("Ci='$Ci'");
+	}
+	
+	function registrarAlumno($values){
+		$this->insertRow($values,1);	
+	}
+}
+?>

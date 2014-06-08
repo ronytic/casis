62300168a51be10233cadb45cc98deac2186abff
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-06-07 23:53:25 -0400

    Corrección de Config

--- config.php
+++ config.php
@@ -2,7 +2,9 @@
 $title=utf8_encode("CASIS");
 $anio="2013";
 $directory="casis"; //ej:carp/ sin el "/"
-$url="http://11.1.2.1/";//ej:http://localhost/
-$titulo="C A S I S - 2013";
+$url="http://11.1.2.31/";//ej:http://localhost/
+$titulo="C A S I S - 2014";
+$anuncio='DESARROLLO DE SOFTWARE A MEDIDA - DESARROLLO DE SISTEMAS WEB - SISTEMAS DE VENTAS E INVENTARIOS - SISTEMA DE FACTURACIÓN - PÁGINAS WEB - INSTALACIÓN DE SISTEMAS DE SEGURIDAD';
+$contacto='CONTACTOS:  <img src="../images/telefono.png" width="15"> 73230568  - <img src="../images/whatsapp.png" width="20"> 73230568 <br> <img src="../images/fb.png" width="15"> /ronaldnina - <img src="../images/twitter.png" width="15">@ronytic';
 $cantidadDias="4";
 ?>

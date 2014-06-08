f3bd8b1ab3a402328e645cc88dbf27b5c5aae1a8
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-06-07 20:48:51 -0400

    Anuncio pie de Página

---
+++ pieanuncio.php
@@ -0,0 +1,9 @@
+<div class="grid_8 piepagina" style="" >
+    <div id="anuncio" class="">
+    <?php echo $anuncio?>
+    </div>
+    
+</div>
+<div class="grid_4 contacto der" style="right:0px;">
+    <?php echo $contacto?>
+</div>

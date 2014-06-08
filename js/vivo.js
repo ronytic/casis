f4c55902370c74ab245358f59fe3671ac81785ca
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-06-07 23:54:08 -0400

    Correción de Vivo.js

--- js/vivo.js
+++ js/vivo.js
@@ -14,4 +14,17 @@ $(document).ready(function(e) {
 		$("#ultimos").html(data);
 		setTimeout(actualizar,2000);
 	}
+	
+	$('#anuncio').marquee({
+		//speed in milliseconds of the marquee
+    duration: 15000,
+    //gap in pixels between the tickers
+    gap: 50,
+    //time in milliseconds before the marquee will start animating
+    delayBeforeStart: 0,
+    //'left' or 'right'
+    direction: 'left',
+    //true or false - should the marquee be duplicated to show an effect of continues flow
+    duplicated: true
+	});
 });

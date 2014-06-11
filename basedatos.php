b3c1bdec8c1d0113252534cef3d5cb9f117fe1ee
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-06-11 16:45:52 -0400

    Correción Base de Datos

--- basedatos.php
+++ basedatos.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('America/La_Paz');
 setlocale(LC_CTYPE, "es_ES");
 setlocale(LC_ALL, 'sp'); 
-setlocale(LC_ALL, 'esp'); 
+setlocale(LC_ALL,"es_ES@euro","es_ES","esp");
 $host="localhost";
 $user="root";
 $pass="";
